dd71d1008203aafd92ec13a0ecd9d99de9829dfc
rtsoliday <soliday@aps.anl.gov>
2025-04-16 14:09:41 -0500

    Added files to create RPMs

---
+++ rpm/build.tcl
@@ -0,0 +1,32 @@
+#!/bin/sh  
+# \
+exec tclsh "$0" "$@"
+
+set dir [pwd]
+
+set version 4.9.0
+set name spiffe-$version
+puts "Building $name RPM"
+
+exec ./rpmdev-setuptree
+exec cp -f spiffe.spec $env(HOME)/rpmbuild/SPECS/
+exec rm -rf $env(HOME)/rpmbuild/BUILD/$name
+exec mkdir $env(HOME)/rpmbuild/BUILD/$name
+set binFiles [glob ../bin/Linux-x86_64/*]
+foreach f $binFiles {
+  exec chmod a+rx $f
+  exec chmod a-w $f
+  exec cp -f $f $env(HOME)/rpmbuild/BUILD/${name}/
+}
+cd $env(HOME)/rpmbuild/BUILD
+exec tar -cvf ../SOURCES/${name}.tar $name
+exec rm -f ../SOURCES/${name}.tar.gz
+exec gzip -9 ../SOURCES/${name}.tar
+cd ../SPECS
+if {[catch {exec rpmbuild -bb --quiet --clean --target x86_64 \
+                 --buildroot $env(HOME)/rpmbuild/BUILDROOT spiffe.spec} results]} {
+}
+exec rm -f ../SOURCES/${name}.tar.gz
+puts $results
+
+
---
+++ rpm/spiffe.spec
@@ -0,0 +1,32 @@
+Summary:	Accelerator code
+Name:		spiffe
+License:	EPICS Open license http://www.aps.anl.gov/epics/license/open.php
+Group:		Applications/Databases
+URL:		http://www.aps.anl.gov/asd/oag/oaghome.shtml
+Packager:	Robert Soliday <soliday@aps.anl.gov>
+Prefix:		%{_bindir}
+Autoreq:	0
+Version:	4.9.0
+Release:	1
+Source:		spiffe-4.9.0.tar.gz
+
+
+%define debug_package %{nil}
+%undefine __check_files
+%description
+Binary package for Spiffe. Spiffe is a fully electromagnetic 2-1/2
+dimensional particle-in-cell code for simulation of rf guns
+and similar systems with cylindrical symmetry.
+
+%prep
+%setup
+
+%build
+%install
+mkdir -p %{buildroot}%{_bindir}
+install -s -m 755 spiffe %{buildroot}%{_bindir}/spiffe
+
+%files
+
+%{_bindir}/spiffe
+
